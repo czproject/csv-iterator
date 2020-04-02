9b403363292b883d9a7208ebaad3d4b8766fde3d
Jan Pecha <janpecha@email.cz>
2020-04-02 17:34:33 +0200

    Improved BOM handling

--- src/CsvIterator.php
+++ src/CsvIterator.php
@@ -177,6 +177,10 @@
 					throw new IOException('File \'' . $this->file . '\' not found or access denied.');
 				}
 
+				if ($this->encoding === self::ENCODING_UTF_8 && fgets($f, 4) !== "\xEF\xBB\xBF") {
+					rewind($f);
+				}
+
 				$this->pointer = $f;
 				$this->inProgress = TRUE;
 			}
@@ -187,9 +191,6 @@
 		{
 			if ($this->encoding !== self::ENCODING_UTF_8) {
 				$value = iconv($this->encoding, 'UTF-8//TRANSLIT', $value);
-
-			} else {
-				$value = str_replace("\xEF\xBB\xBF", '', $value); // remove BOM
 			}
 
 			if ($this->escape !== '' && $this->enclosure !== '') { // fgetcsv() dosn't return unescaped strings, see http://php.net/manual/en/function.fgetcsv.php#119896
